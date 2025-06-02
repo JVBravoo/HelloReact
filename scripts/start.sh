24b75e5804e04e6dd674d34427beaa760c528238
João Victor Bravo <jvbm@cesar.school>
2025-06-02 16:49:32 -0300

    Create start.sh

---
+++ scripts/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Navega até a pasta da aplicação
+cd /home/ubuntu/HelloReact
+
+# Instala dependências (opcional)
+npm install
+
+# Inicia a aplicação com PM2 (ou node direto se preferir)
+pm2 start app.js --name "HelloReact"
