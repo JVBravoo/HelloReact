0085e3caa24ae02c9553bed3d0bb2fd9ee2024a6
João Victor Bravo <joaovictorbravo804@gmail.com>
2025-06-04 11:37:06 -0300

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-# Para a aplicação apenas se estiver rodando
-pm2 stop HelloReact || echo "Aplicação HelloReact não estava rodando"
-pm2 delete HelloReact || echo "Aplicação HelloReact não estava registrada"
+pm2 stop HelloReact || true
