4810e694096c125f04ed169e643bfdcdac7e05cd
João Victor Bravo <joaovictorbravo804@gmail.com>
2025-06-04 14:06:57 -0300

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
+nomeApp="HelloReact"
+
 cd /home/ubuntu/HelloReact
-
-# Corrige permissões
-sudo chown -R ubuntu:ubuntu .
-
-# Instala dependências
 npm install
 
-# Compila o projeto com compatibilidade OpenSSL
-NODE_OPTIONS=--openssl-legacy-provider npm run build
-
-# Inicia com o 'serve' (modo produção)
-pm2 start "serve -s build" --name HelloReact
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl start $nomeApp
