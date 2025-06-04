8d12f6efc4a4582502fa9a49f82017008fbb99c2
João Victor Bravo <joaovictorbravo804@gmail.com>
2025-06-04 10:51:12 -0300

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
+nomeApp="HelloReact"
 
-# Try to stop and delete the app, but don't fail if it doesn't exist
-pm2 stop HelloReact || true
-pm2 delete HelloReact || true
+sudo supervisorctl stop $nomeApp
